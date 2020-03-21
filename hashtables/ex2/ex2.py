4bcfd277d0a48755e2c479006ea6be24e5b67bf9
fritzgt <tuanis@live.com>
2020-03-21 04:44:57 +0200

    added items to ht

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,25 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # Each ticket is represented as an instance of Class Ticket
+    for ticket in tickets:
+        source = ticket.source
+        destination = ticket.destination
+        print(f"source: {source}")
+        print(f"destination: {destination}")
+        # Add items to the hash table
+        # Starting location = key / the destination = value
+        hash_table_insert(hashtable, source, destination)
+
+    # Reconstruct the intinerary with the correct link order in an array
 
     pass
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, 3)
